323f9aaa8044276c71ea42cedcc38e14c6cdffc1
oleh <olegov23445@gmail.com>
2023-05-11 05:53:09 +0300

    fix

--- app/js/dist/main.dev.js
+++ app/js/dist/main.dev.js
@@ -1,8 +1,16 @@
 "use strict";
 
+document.addEventListener('DOMContentLoaded', function (event) {
+  setTimeout(function () {
+    document.querySelector('.loading').classList.remove('loading');
+    document.querySelector('.lds-ellipsis').classList.remove('lds-ellipsis');
+  }, 3000);
+});
 window.addEventListener('load', function () {
   burgerButton();
   activeButton();
+  buttonWhatWeDo();
+  loading();
 });
 var button = document.querySelector('.footer__subscribe-button');
 var input = document.querySelector('.footer__subscribe-input');
@@ -28,7 +36,8 @@ function activeButton() {
         icon: 'success',
         title: 'Successfully',
         showConfirmButton: false,
-        timer: 1500
+        timer: 1500,
+        scrollbarPadding: false
       });
     } else {
       Swal.fire({
@@ -36,7 +45,9 @@ function activeButton() {
         icon: 'error',
         title: 'Error',
         showConfirmButton: false,
-        timer: 1000
+        timer: 1000,
+        scrollbarPadding: false,
+        allowOutsideClick: false
       });
       input.form.reset();
     }
@@ -59,4 +70,114 @@ function burgerButton() {
       }
     });
   });
+}
+
+function buttonWhatWeDo() {
+  var buttons = document.querySelectorAll('.whatwedo__link');
+  buttons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      var value = button.dataset.number;
+
+      switch (value) {
+        case '1':
+          Swal.fire({
+            title: 'Product Photography',
+            html: 'Cras commodo consequat orci, in convallis risus egestas non. Nulla efficitur auctor hendrerit. Etiam ut orci varius, faucibus libero ac, cursus quam.<br><br><a href="https://example.com" target="_blank" class="whatwedo__link-modal">Link</a>',
+            imageUrl: '../../images/whatwedo-img1.jpg',
+            imageWidth: 300,
+            imageHeight: 300,
+            background: 'black',
+            color: 'white',
+            confirmButtonColor: 'gray',
+            scrollbarPadding: false,
+            allowOutsideClick: false
+          });
+          break;
+
+        case '2':
+          Swal.fire({
+            title: 'Architecture Photography',
+            html: 'Aenean porta neque eget consequat fringilla. Vestibulum ultrices, orci nec egestas pharetra, ligula est semper enim, nec auctor sapien leo nec purus. Fusce tincidunt aliquet sapien, sit amet rhoncus leo imperdiet nec.<br><br><a href="https://example.com" target="_blank" class="whatwedo__link-modal">Link</a>',
+            imageUrl: '../../images/whatwedo-img2.jpg',
+            imageWidth: 300,
+            imageHeight: 300,
+            background: 'black',
+            color: 'white',
+            confirmButtonColor: 'gray',
+            scrollbarPadding: false,
+            allowOutsideClick: false
+          });
+          break;
+
+        case '3':
+          Swal.fire({
+            title: 'Drone Photography',
+            html: 'Mauris euismod elit et nisi ultrices, ut faucibus orci tincidunt. Duis tristique sed lorem a vestibulum. Cras commodo consequat orci, in convallis risus egestas non. Nulla efficitur auctor hendrerit. Etiam ut orci varius, faucibus libero ac, cursus quam.<br><br><a href="https://example.com" target="_blank" class="whatwedo__link-modal">Link</a>',
+            imageUrl: '../../images/whatwedo-img3.jpg',
+            imageWidth: 300,
+            imageHeight: 300,
+            background: 'black',
+            color: 'white',
+            confirmButtonColor: 'gray',
+            scrollbarPadding: false,
+            allowOutsideClick: false
+          });
+          break;
+
+        case '4':
+          Swal.fire({
+            title: 'WILDLIFE PHOTOGRAPHY',
+            html: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque aliquam odio et faucibus. Nulla rhoncus feugiat eros quis consectetur. Morbi neque ex, condimentum dapibus congue et, vulputate ut ligula. Vestibulum sit amet urna turpis.<br><br><a href="https://example.com" target="_blank" class="whatwedo__link-modal">Link</a>',
+            imageUrl: '../../images/whatwedo-img4.jpg',
+            imageWidth: 300,
+            imageHeight: 300,
+            background: 'black',
+            color: 'white',
+            confirmButtonColor: 'gray',
+            scrollbarPadding: false,
+            allowOutsideClick: false
+          });
+          break;
+
+        default:
+          break;
+      }
+    });
+  });
+}
+
+var anchors = document.querySelectorAll('a[href*="#"]');
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  var _loop = function _loop() {
+    var anchor = _step.value;
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      var blockID = anchor.getAttribute('href').substr(1);
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  };
+
+  for (var _iterator = anchors[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    _loop();
+  }
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+      _iterator["return"]();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }
 }
--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,15 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  setTimeout(() => {
+    document.querySelector('.loading').classList.remove('loading')
+    document.querySelector('.lds-ellipsis').classList.remove('lds-ellipsis')
+  }, 3000)
+});
+
 window.addEventListener('load',function(){
     burgerButton()
     activeButton()
+    buttonWhatWeDo()
+    loading()
 })
 
 const button = document.querySelector('.footer__subscribe-button')
@@ -27,39 +36,124 @@ function activeButton(){
                 icon: 'success',
                 title: 'Successfully',
                 showConfirmButton: false,
-                timer: 1500
+                timer: 1500,
+                scrollbarPadding: false,
               })
-
         } else {
             Swal.fire({
                 position: 'center',
                 icon: 'error',
                 title: 'Error',
                 showConfirmButton: false,
-                timer: 1000
+                timer: 1000,
+                scrollbarPadding: false,
+                allowOutsideClick: false
               })
             input.form.reset(); 
         }
-        
     })
 }
 function burgerButton() {
-    const burger = document.querySelector('.burger');
-    const menu = document.querySelector('.header__menu-list');
-    const link = document.querySelectorAll('.header__munu-link');
-  
-    burger.addEventListener('click', function() {
-      menu.classList.toggle('header__menu-list--active');
-      burger.classList.toggle('burger--active');
-    });
-  
-    link.forEach(function(item){
-      item.addEventListener('click', function(){
-        if(menu){
-          menu.classList.remove('header__menu-list--active');
-          burger.classList.remove('burger--active');
-        }
-      })
+  const burger = document.querySelector('.burger');
+  const menu = document.querySelector('.header__menu-list');
+  const link = document.querySelectorAll('.header__munu-link');
+
+  burger.addEventListener('click', function() {
+    menu.classList.toggle('header__menu-list--active');
+    burger.classList.toggle('burger--active');
+  });
+
+  link.forEach(function(item){
+    item.addEventListener('click', function(){
+      if(menu){
+        menu.classList.remove('header__menu-list--active');
+        burger.classList.remove('burger--active');
+      }
     })
-  
-  }
+  })
+
+}
+function buttonWhatWeDo() {
+  const buttons = document.querySelectorAll('.whatwedo__link');
+  buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      const value = button.dataset.number;
+      switch (value) {
+        case '1':
+          Swal.fire({
+            title: 'Product Photography',
+            html: 'Cras commodo consequat orci, in convallis risus egestas non. Nulla efficitur auctor hendrerit. Etiam ut orci varius, faucibus libero ac, cursus quam.<br><br><a href="https://example.com" target="_blank" class="whatwedo__link-modal">Link</a>',
+            imageUrl: '../../images/whatwedo-img1.jpg',
+            imageWidth: 300,
+            imageHeight: 300,
+            background: 'black',
+            color: 'white',
+            confirmButtonColor: 'gray',
+            scrollbarPadding: false,
+            allowOutsideClick: false,
+          });
+          break;
+        case '2':
+          Swal.fire({
+            title: 'Architecture Photography',
+            html: 'Aenean porta neque eget consequat fringilla. Vestibulum ultrices, orci nec egestas pharetra, ligula est semper enim, nec auctor sapien leo nec purus. Fusce tincidunt aliquet sapien, sit amet rhoncus leo imperdiet nec.<br><br><a href="https://example.com" target="_blank" class="whatwedo__link-modal">Link</a>',
+            imageUrl: '../../images/whatwedo-img2.jpg',
+            imageWidth: 300,
+            imageHeight: 300,
+            background: 'black',
+            color: 'white',
+            confirmButtonColor: 'gray',
+            scrollbarPadding: false,
+            allowOutsideClick: false,
+          });
+          break;
+          case '3':
+            Swal.fire({
+              title: 'Drone Photography',
+              html: 'Mauris euismod elit et nisi ultrices, ut faucibus orci tincidunt. Duis tristique sed lorem a vestibulum. Cras commodo consequat orci, in convallis risus egestas non. Nulla efficitur auctor hendrerit. Etiam ut orci varius, faucibus libero ac, cursus quam.<br><br><a href="https://example.com" target="_blank" class="whatwedo__link-modal">Link</a>',
+              imageUrl: '../../images/whatwedo-img3.jpg',
+              imageWidth: 300,
+              imageHeight: 300,
+              background: 'black',
+              color: 'white',
+              confirmButtonColor: 'gray',
+              scrollbarPadding: false,
+              allowOutsideClick: false,             
+            });
+            break;
+            case '4':
+              Swal.fire({
+                title: 'WILDLIFE PHOTOGRAPHY',
+                html: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque aliquam odio et faucibus. Nulla rhoncus feugiat eros quis consectetur. Morbi neque ex, condimentum dapibus congue et, vulputate ut ligula. Vestibulum sit amet urna turpis.<br><br><a href="https://example.com" target="_blank" class="whatwedo__link-modal">Link</a>',
+                imageUrl: '../../images/whatwedo-img4.jpg',
+                imageWidth: 300,
+                imageHeight: 300,
+                background: 'black',
+                color: 'white',
+                confirmButtonColor: 'gray',
+                scrollbarPadding: false,
+                allowOutsideClick: false,
+              
+              });
+              break;
+        default:
+          break;
+      }
+    });
+  });
+}
+const anchors = document.querySelectorAll('a[href*="#"]')
+    
+    for (let anchor of anchors) {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+        
+        const blockID = anchor.getAttribute('href').substr(1)
+        
+        document.getElementById(blockID).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        })
+      })
+    }
+    
